20eda9495a21db2ba3dfeeda3b2b5b75e995cbf9
Rom Grk <romgrk.cc@gmail.com>
2026-01-13 13:12:53 -0500

    refactor: setup generator

---
+++ src/generate.js
@@ -0,0 +1,34 @@
+import fs from 'node:fs';
+import path from 'node:path';
+
+const INPUT_PATH = path.join(import.meta.dirname, 'content');
+const OUTPUT_PATH = path.join(import.meta.dirname, '..');
+
+generate();
+
+function generate() {
+  const inputFiles = fs.readdirSync(INPUT_PATH);
+
+  inputFiles.forEach((file) => {
+    if (file.startsWith('_')) {
+      return;
+    }
+
+    const inputFilePath = path.join(INPUT_PATH, file);
+    const outputFilePath = path.join(OUTPUT_PATH, file);
+
+    const content = fs.readFileSync(inputFilePath, 'utf-8');
+    const transformedContent = content.replace(/{{{([^}]+)}}}/g, (_, templatePath) => {
+      const inputDirname = path.dirname(inputFilePath);
+      const templateContent = fs.readFileSync(
+        path.join(inputDirname, templatePath.trim()),
+        'utf-8',
+      );
+
+      return templateContent;
+    });
+
+    fs.writeFileSync(outputFilePath, transformedContent, 'utf-8');
+    console.log(`Generated: ${outputFilePath}`);
+  });
+}

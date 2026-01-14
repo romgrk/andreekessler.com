2f5e888313c018cf5a62e4625ea4111586bfe379
Rom Grk <romgrk.cc@gmail.com>
2026-01-14 17:12:26 -0500

    fix: script

--- static/index.js
+++ static/index.js
@@ -5,10 +5,26 @@ document.addEventListener('DOMContentLoaded', () => {
       project.querySelector('.project__images').querySelectorAll('img, video'),
     );
 
-    console.log(images);
-    images[0].addEventListener('load', () => {
-      const height = images[0].clientHeight;
-      console.log({ height });
+    images[0].addEventListener('load', async () => {
+      let tries = 0;
+      let height = 0;
+      while (true) {
+        height = images[0].clientHeight;
+
+        if (height > 0) {
+          break;
+        }
+
+        tries += 1;
+
+        if (tries >= 10) {
+          console.warn('Could not set image heights correctly.');
+          height = 500;
+          break;
+        }
+        await new Promise((resolve) => setTimeout(resolve, 50));
+      }
+
       images.forEach((img) => {
         img.style.height = `${height}px`;
       });

aae1999b8af30b653519bf5f50f269ab890cd2c2
Rom Grk <romgrk.cc@gmail.com>
2026-01-14 17:56:46 -0500

    feat: blur stuff

--- static/index.js
+++ static/index.js
@@ -5,29 +5,37 @@ document.addEventListener('DOMContentLoaded', () => {
       project.querySelector('.project__images').querySelectorAll('img, video'),
     );
 
-    images[0].addEventListener('load', async () => {
-      let tries = 0;
-      let height = 0;
-      while (true) {
-        height = images[0].clientHeight;
-
-        if (height > 0) {
-          break;
-        }
-
-        tries += 1;
-
-        if (tries >= 10) {
-          console.warn('Could not set image heights correctly.');
-          height = 500;
-          break;
-        }
-        await new Promise((resolve) => setTimeout(resolve, 50));
-      }
-
-      images.forEach((img) => {
-        img.style.height = `${height}px`;
+    if (images[0].complete) {
+      inferHeightFromFirstImage(images);
+    } else {
+      images[0].addEventListener('load', () => {
+        inferHeightFromFirstImage(images);
       });
-    });
+    }
   });
 });
+
+async function inferHeightFromFirstImage(images) {
+  let tries = 0;
+  let height = 0;
+  while (true) {
+    height = images[0].clientHeight;
+    if (height > 0) {
+      break;
+    }
+
+    if (tries++ >= 10) {
+      break;
+    }
+    await new Promise((resolve) => setTimeout(resolve, 50));
+  }
+
+  if (height === 0) {
+    console.warn('Could not set image heights correctly.');
+    height = 500;
+    debugger;
+  }
+  images.forEach((img) => {
+    img.style.height = `${height}px`;
+  });
+}

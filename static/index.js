923ff561960011308c5b226ff39116f8fd5d162b
Rom Grk <romgrk.cc@gmail.com>
2026-01-15 04:39:23 -0500

    feat: navbar indicator

--- static/index.js
+++ static/index.js
@@ -1,3 +1,7 @@
+/*******************************/
+/* Project images height setup */
+/*******************************/
+
 document.addEventListener('DOMContentLoaded', setupImageHeights);
 window.addEventListener('resize', setupImageHeights);
 
@@ -41,4 +45,121 @@ async function inferHeightFromFirstImage(images) {
   images.slice(1).forEach((img) => {
     img.style.height = `${height}px`;
   });
+}
+
+/********************/
+/* Navbar indicator */
+/********************/
+
+const ANCHORS_ID = ['accueil', 'a-propos', 'projets', 'contact'];
+
+document.addEventListener('DOMContentLoaded', setupNavbarIndicator);
+
+function setupNavbarIndicator() {
+  const indicator = document.querySelector('.navbar__indicator');
+  const links = Array.from(document.querySelectorAll('.navbar a'));
+  const anchors = ANCHORS_ID.map((id) => document.getElementById(id));
+  const baseWidth = parseFloat(window.getComputedStyle(indicator).width);
+
+  let animation;
+  let targetIndex = null;
+
+  function updateIndicator(initial = false) {
+    const scrollY = window.scrollY;
+    let newIndex = 0;
+    for (let i = 0; i < anchors.length; i++) {
+      if (scrollY >= anchors[i].offsetTop - 300) {
+        newIndex = i;
+      } else {
+        break;
+      }
+    }
+
+    if (newIndex === targetIndex) {
+      return;
+    }
+
+    targetIndex = newIndex;
+
+    const currentLink = links[newIndex];
+    const width = currentLink.clientWidth;
+
+    const targetLeft = currentLink.offsetLeft + width / 2;
+
+    if (initial === true) {
+      indicator.style.left = `${targetLeft}px`;
+      return;
+    }
+
+    const initialLeft = parseFloat(indicator.style.left);
+
+    animation?.cancel();
+    animation = animate(
+      {
+        duration: 300,
+      },
+      (f) => {
+        indicator.style.left = `${lerp(f, initialLeft, targetLeft)}px`;
+        /* Animate witdth to be wider at the mid-point */
+        const midPoint = 0.5;
+        const maxWidthFactor = 8;
+        let widthFactor;
+        if (f < midPoint) {
+          widthFactor = lerp(f / midPoint, 1, maxWidthFactor);
+        } else {
+          widthFactor = lerp((f - midPoint) / midPoint, maxWidthFactor, 1);
+        }
+        indicator.style.width = `${baseWidth * widthFactor}px`;
+      },
+    );
+  }
+
+  window.addEventListener('scroll', updateIndicator);
+  window.addEventListener('resize', updateIndicator);
+
+  updateIndicator(true);
+}
+
+const easing = (t) => t * t * (3 - 2 * t);
+/**
+ * Animate a value using `requestAnimationFrame()`. This function does not
+ * call `options.onChange` in the current event loop cycle. It is not guaranteed
+ * to stop exactly after `options.duration`, but it does guarantee that it will
+ * never call `options.onChange` with a value outside `options.to`.
+ */
+function animate(options, onChange) {
+  const { from = 0, to = 1, duration = 250, delay = 0 } = options;
+  const start = performance.now();
+  let id = 0;
+
+  let resolve;
+  const promise = new Promise((_resolve) => {
+    resolve = _resolve;
+  });
+
+  const step = (timestamp) => {
+    const elapsed = timestamp - start - delay;
+
+    if (elapsed < 0) {
+      id = requestAnimationFrame(step);
+    } else if (elapsed >= duration) {
+      onChange(to, true);
+      resolve();
+    } else {
+      onChange(lerp(easing(elapsed / duration), from, to), false);
+      id = requestAnimationFrame(step);
+    }
+  };
+
+  promise.cancel = () => {
+    cancelAnimationFrame(id);
+  };
+
+  id = requestAnimationFrame(step);
+
+  return promise;
+}
+
+function lerp(factor, a, b) {
+  return a * (1 - factor) + b * factor;
 }
